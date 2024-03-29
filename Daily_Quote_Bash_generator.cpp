f709d7043be6dcb0d88747e4b2018617adb48848
haoranwei <haoranwei@outlook.com>
2024-03-29 09:24:14 +0800

    Add the function: randomly generating one quote from quotes.json

--- Daily_Quote_Bash_generator.cpp
+++ Daily_Quote_Bash_generator.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <ctime> // For time()
+#include <cstdlib> // For srand() and rand()
 #include "include/rapidjson/document.h"
 #include "include/rapidjson/filereadstream.h"
+
+// Function to generate a random number within a range
+int getRandomNumber(int min, int max) {
+    static bool firstTime = true;
+    if (firstTime) {
+        srand(time(NULL)); // Seed the random number generator
+        firstTime = false;
+    }
+    return min + rand() % ((max + 1) - min);
+}
 
 int main() {
     // Read the JSON file
@@ -15,10 +28,16 @@ int main() {
 
     // Extract quotes from the JSON document
     const rapidjson::Value& quotes = document["quotes"];
+    std::vector<std::string> allQuotes;
+
     if (quotes.IsArray()) {
         for (rapidjson::SizeType i = 0; i < quotes.Size(); i++) {
-            std::cout << quotes[i].GetString() << std::endl;
+            allQuotes.push_back(quotes[i].GetString());
         }
+        
+        // Select a random quote and print it
+        int randomIndex = getRandomNumber(0, allQuotes.size() - 1);
+        std::cout << "Random Quote: " << allQuotes[randomIndex] << std::endl;
     }
 
     return 0;
